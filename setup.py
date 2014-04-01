5f4749f8e26da76541e6e7ab12e732687ba90658
Nathan Stocks <nathan.stocks@gmail.com>
2014-04-01 17:02:20 -0600

    Starting to make a setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+    setup(
+        name = 'green',
+        packages = ['green'],
+        version = '0.1',
+        description = 'A plugin for nose that provides the colored, aligned, clean output that nose ought to have by default.',
+        author = 'Nathan Stocks',
+        author_email = 'nathan.stocks@gmail.com',
+        url = 'https://github.com/CleanCut/green',
+        download_url = '',
+        keywords = [''],
+        classifiers = [],
+    )
