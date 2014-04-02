89849d7d0365794a03b35cd4becab9766a3acb72
Nathan Stocks <nathan.stocks@gmail.com>
2014-04-01 19:19:41 -0600

    Fixed setup.py so that the version is stored in a variable and all references to it use the variable.  Bumped version to 0.3 as well.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from distutils.core import setup
 
+VERSION='0.3'
+
 setup(
     name = 'green',
     packages = ['green'],
-    version = '0.2',
+    version = VERSION,
     description = 'A plugin for nose that provides the colored, aligned, clean output that nose ought to have by default.',
     author = 'Nathan Stocks',
     author_email = 'nathan.stocks@gmail.com',
     license = 'MIT',
     url = 'https://github.com/CleanCut/green',
-    download_url = 'https://github.com/CleanCut/green/tarball/0.1',
+    download_url = 'https://github.com/CleanCut/green/tarball/' + VERSION,
     keywords = ['nose', 'nosetest', 'nosetests', 'plugin', 'green', 'test', 'unittest', 'color', 'tabular', 'clean', 'red', 'rednose'],
     classifiers = [
         'Development Status :: 2 - Pre-Alpha',
