76c98206a98e10c916656e1b3fec92883a33ed40
Nathan Stocks <nathan.stocks@gmail.com>
2014-04-06 13:57:58 -0600

    Updated the options test to match with the updated environment variable.

--- green/test/test_plugin.py
+++ green/test/test_plugin.py
@@ -18,7 +18,7 @@ class TestPlugin(PluginTester, unittest.TestCase):
     def test_NOSE_GREEN_setsEnabled(self):
         parser = MagicMock()
         self.assertEqual(self.g.enabled, False)
-        self.g.options(parser, env={'NOSE_GREEN':'1'})
+        self.g.options(parser, env={'NOSE_WITH_GREEN':'1'})
         self.assertEqual(self.g.enabled, True)
 
 
