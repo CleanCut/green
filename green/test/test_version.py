69681bacd6b6ae55865e9aba2041b3f12057c411
Nathan Stocks <nathan.stocks@gmail.com>
2014-05-08 22:15:35 -0600

    Set docstrings for the version tests.

--- green/test/test_version.py
+++ green/test/test_version.py
@@ -7,8 +7,10 @@ from green.version import __version__
 class TestVersion(unittest.TestCase):
 
     def test_version_type(self):
+        "__version__ is a string"
         self.assertEqual(type(__version__), type(''))
 
 
     def test_version_set(self):
+        "__version__ is not blank"
         self.assertTrue(len(__version__) > 0)
