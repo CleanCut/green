da32325f01f3530a2e2e48a62d2d64c0b6e2cdf2
Nathan Stocks <nathan.stocks@gmail.com>
2014-05-12 22:21:06 -0600

    Added a test for version.pretty_version().  Added a 'pragma nocover' for the absence-of-coverage branch, since modules can't be unloaded and I can't figure out a good way to test it.

--- green/test/test_version.py
+++ green/test/test_version.py
@@ -1,16 +1,26 @@
 import unittest
 
-from green.version import __version__
+from green.version import __version__, pretty_version
+import green.version
 
 
 
 class TestVersion(unittest.TestCase):
 
-    def test_version_type(self):
+    def test_versionType(self):
         "__version__ is a string"
         self.assertEqual(type(__version__), type(''))
 
 
-    def test_version_set(self):
+    def test_versionSet(self):
         "__version__ is not blank"
         self.assertTrue(len(__version__) > 0)
+
+
+    def test_pretty_version(self):
+        pv = pretty_version()
+        self.assertTrue('Green' in pv)
+        self.assertTrue('Python' in pv)
+        if green.version.coverage:
+            self.assertTrue('Coverage' in pv)
+
--- green/version.py
+++ green/version.py
@@ -3,7 +3,7 @@ import sys
 
 try:
     import coverage
-except:
+except: # pragma nocover
     coverage = None
 
 __version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read().strip()
