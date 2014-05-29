e6fd23e9ec84d8658f1b7c9df7f14ad065dfcc3d
Nathan Stocks <nathan.stocks@gmail.com>
2014-05-28 23:05:50 -0600

    Split ProtoTestResult and GreenTestResult into a result module.  Did the same with the corresponding tests.

---
+++ green/result.py
@@ -0,0 +1,333 @@
+from __future__ import unicode_literals
+import time
+import traceback
+
+from green.output import Colors, debug
+from green.version import pretty_version
+
+try: # pragma nocover
+    import html
+    escape = html.escape
+except: # pragma nocover
+    import cgi
+    escape = cgi.escape
+
+
+
+class ProtoTestResult():
+
+
+    def __init__(self):
+        self.shouldStop = False
+        # Individual lists
+        self.errors              = []
+        self.expectedFailures    = []
+        self.failures            = []
+        self.passing             = []
+        self.skipped             = []
+        self.unexpectedSuccesses = []
+
+
+    def __repr__(self): # pragma: no cover
+        return (
+                "errors" + str(self.errors) + ', ' +
+                "expectedFailures" + str(self.expectedFailures) + ', ' +
+                "failures" + str(self.failures) + ', ' +
+                "passing" + str(self.passing) + ', ' +
+                "skipped" + str(self.skipped) + ', ' +
+                "unexpectedSuccesses" + str(self.unexpectedSuccesses))
+
+
+    def startTest(self, test):
+        "Called before each test runs"
+
+
+    def stopTest(self, test):
+        "Called after each test runs"
+
+
+    def addSuccess(self, test):
+        "Called when a test passed"
+        self.passing.append(test)
+
+
+    def addError(self, test, err):
+        "Called when a test raises an exception"
+        self.errors.append((test, traceback.format_exception(*err)))
+
+
+    def addFailure(self, test, err):
+        "Called when a test fails a unittest assertion"
+        self.failures.append((test, traceback.format_exception(*err)))
+
+
+    def addSkip(self, test, reason):
+        "Called when a test is skipped"
+        self.skipped.append((test, reason))
+
+
+    def addExpectedFailure(self, test, err):
+        "Called when a test fails, and we expeced the failure"
+        self.expectedFailures.append((test, traceback.format_exception(*err)))
+
+
+    def addUnexpectedSuccess(self, test):
+        "Called when a test passed, but we expected a failure"
+        self.unexpectedSuccesses.append(test)
+
+
+
+class GreenTestResult():
+    "Aggregates test results and outputs them to a stream."
+
+
+    def __init__(self, stream, descriptions, verbosity, html=False,
+            termcolor=None):
+        """stream, descriptions, and verbosity are as in
+        unittest.runner.TextTestRunner.
+        """
+        self.stream       = stream
+        self.showAll      = verbosity > 1
+        self.dots         = verbosity == 1
+        self.verbosity    = verbosity
+        self.descriptions = descriptions
+        self.colors       = Colors(termcolor, html)
+        self.last_module  = ''
+        self.last_class   = ''
+        self.shouldStop   = False
+        self.testsRun     = 0
+        # Individual lists
+        self.errors              = []
+        self.expectedFailures    = []
+        self.failures            = []
+        self.passing             = []
+        self.skipped             = []
+        self.unexpectedSuccesses = []
+        # Combination of all errors and failures
+        self.all_errors = []
+
+
+    def addProto(self, proto):
+        if getattr(proto, 'passing', None) == None:
+            print(proto)
+        else:
+            for test, err in proto.errors:
+                self.startTest(test)
+                self.addError(test, err)
+            for test, err in proto.expectedFailures:
+                self.startTest(test)
+                self.addExpectedFailure(test, err)
+            for test, err in proto.failures:
+                self.startTest(test)
+                self.addFailure(test, err)
+            for test in proto.passing:
+                self.startTest(test)
+                self.addSuccess(test)
+            for test, reason in proto.skipped:
+                self.startTest(test)
+                self.addSkip(test, reason)
+            for test in proto.unexpectedSuccesses:
+                self.startTest(test)
+                self.addUnexpectedSuccess(test)
+
+
+    def startTestRun(self):
+        "Called once before any tests run"
+        self.startTime = time.time()
+        # Really verbose information
+        if self.colors.html:
+            self.stream.write(
+                    '<div style="font-family: Monaco, \'Courier New\', monospace; color: rgb(170,170,170); background: rgb(0,0,0); padding: 14px;">')
+        if self.verbosity > 2:
+            self.stream.writeln(self.colors.bold(pretty_version() + "\n"))
+
+
+    def stopTestRun(self):
+        "Called once after all tests have run"
+        self.stopTime = time.time()
+        self.timeTaken = self.stopTime - self.startTime
+        self.printErrors()
+        if self.testsRun:
+            self.stream.writeln()
+        self.stream.writeln("Ran %s test%s in %ss" %
+            (self.colors.bold(str(self.testsRun)),
+            self.testsRun != 1 and "s" or "",
+            self.colors.bold("%.3f" % self.timeTaken)))
+        self.stream.writeln()
+        results = [
+            (self.errors, 'errors', self.colors.error),
+            (self.expectedFailures, 'expected_failures',
+                self.colors.expectedFailure),
+            (self.failures, 'failures', self.colors.failing),
+            (self.passing, 'passes', self.colors.passing),
+            (self.skipped, 'skips', self.colors.skipped),
+            (self.unexpectedSuccesses, 'unexpected_successes',
+                self.colors.unexpectedSuccess),
+        ]
+        stats = []
+        for obj_list, name, color_func in results:
+            if obj_list:
+                stats.append("{}={}".format(name, color_func(str(len(obj_list)))))
+        if not stats:
+            self.stream.writeln(self.colors.passing("No Tests Found"))
+        else:
+            grade = self.colors.passing('OK')
+            if self.errors or self.failures:
+                grade = self.colors.failing('FAILED')
+            self.stream.writeln("{} ({})".format(grade, ', '.join(stats)))
+        if self.colors.html:
+            self.stream.writeln('</div>')
+
+
+    def startTest(self, test):
+        "Called before the start of each test"
+        self.testsRun += 1
+
+        # Get our bearings
+        current_module = test.__module__
+        current_class  = test.__class__.__name__
+
+        # Output
+        if self.showAll:
+            # Module...if it changed.
+            if current_module != self.last_module:
+                self.stream.writeln(self.colors.moduleName(current_module))
+            # Class...if it changed.
+            if current_class != self.last_class:
+                self.stream.writeln(self.colors.className(
+                    self.stream.formatText(current_class, indent=1)))
+            # Test name or description
+            self.test_output_line = self._testDescription(test)
+            if not self.colors.html:
+                # In the terminal, we will write a placeholder, and then
+                # rewrite it in color after the test has run.
+                self.stream.write(
+                    self.colors.bold(
+                        self.stream.formatLine(self.test_output_line, indent=2)))
+            self.stream.flush()
+
+        # Set state for next time
+        if current_module != self.last_module:
+            self.last_module = current_module
+        if current_class != self.last_class:
+            self.last_class = current_class
+
+
+    def stopTest(self, test):
+        "Called after the end of each test"
+
+
+    def _testDescription(self, test):
+        return test.shortDescription() or str(test).split()[0]
+
+
+    def _reportOutcome(self, test, outcome_char, color_func, err=None,
+            reason=''):
+        if self.showAll:
+            # Move the cursor back to the start of the line in terminal mode
+            if not self.colors.html:
+                self.stream.write('\r')
+            # Escape the HTML that may be in the docstring
+            if self.colors.html:
+                self.test_output_line = escape(self.test_output_line)
+            self.stream.write(
+                color_func(
+                    self.stream.formatLine(
+                        self.test_output_line,
+                        indent=2,
+                        outcome_char=outcome_char)
+                )
+            )
+            if reason:
+                self.stream.write(color_func(' -- ' + reason))
+            self.stream.writeln()
+            self.stream.flush()
+        elif self.dots:
+            self.stream.write(color_func(outcome_char))
+            self.stream.flush()
+
+
+    def addSuccess(self, test):
+        "Called when a test passed"
+        self.passing.append(test)
+        self._reportOutcome(test, '.', self.colors.passing)
+
+
+    def addError(self, test, err):
+        "Called when a test raises an exception"
+        self.errors.append(test)
+        self.all_errors.append((test, self.colors.error, 'Error', err))
+        self._reportOutcome(test, 'E', self.colors.error, err)
+
+
+    def addFailure(self, test, err):
+        "Called when a test fails a unittest assertion"
+        self.failures.append(test)
+        self.all_errors.append((test, self.colors.error, 'Failure', err))
+        self._reportOutcome(test, 'F', self.colors.failing, err)
+
+
+    def addSkip(self, test, reason):
+        "Called when a test is skipped"
+        self.skipped.append(test)
+        self._reportOutcome(
+                test, 's', self.colors.skipped, reason=reason)
+
+
+    def addExpectedFailure(self, test, err):
+        "Called when a test fails, and we expeced the failure"
+        self.expectedFailures.append(test)
+        self._reportOutcome(test, 'x', self.colors.expectedFailure, err)
+
+
+    def addUnexpectedSuccess(self, test):
+        "Called when a test passed, but we expected a failure"
+        self.unexpectedSuccesses.append(test)
+        self._reportOutcome(test, 'u', self.colors.unexpectedSuccess)
+
+
+    def printErrors(self):
+        "Print a list of all tracebacks from errors and failures"
+        if not self.all_errors:
+            return
+        if self.dots:
+            self.stream.writeln()
+        for (test, color_func, outcome, err) in self.all_errors:
+            # Header Line
+            self.stream.writeln(
+                    '\n' + color_func(outcome) +
+                    ' in ' + self.colors.bold(str(test).split()[0]) +
+                    ' from ' + str(test).split()[1].strip('()'))
+
+            # Frame Line
+            relevant_frames = []
+            if issubclass(type(err), Exception) or (type(err) == tuple):
+                iterable = enumerate(traceback.format_exception(*err))
+            else:
+                iterable = enumerate(err)
+            for i, frame in iterable:
+                debug('\n' + '*' * 30 + "Frame {}:".format(i) + '*' * 30
+                      + "\n{}".format(self.colors.yellow(frame)), level = 3)
+                # Ignore useless frames
+                if self.verbosity < 4:
+                    if frame.strip() == "Traceback (most recent call last):":
+                        continue
+                reindented_lines = []
+                # If we're in html, space-based indenting needs to be converted.
+                if self.colors.html:
+                    for line in frame.split('\n'):
+                        frame_indent = 0
+                        while line[:2] == '  ':
+                            line = line[2:]
+                            frame_indent += 1
+                        line = self.stream.formatLine(line, indent=frame_indent)
+                        reindented_lines.append(line)
+                    frame = "\n".join(reindented_lines)
+                # Done with this frame, capture it.
+                relevant_frames.append(frame)
+            self.stream.write(''.join(relevant_frames))
+
+
+    def wasSuccessful(self):
+        "Tells whether or not the overall run was successful"
+        return len(self.all_errors) == 0
---
+++ green/test/test_result.py
@@ -0,0 +1,233 @@
+from __future__ import unicode_literals
+import sys
+import traceback
+import unittest
+
+from green.output import GreenStream
+from green.result import GreenTestResult
+
+try:
+    from io import StringIO
+except:
+    from StringIO import StringIO
+
+try:
+    from unittest.mock import MagicMock
+except:
+    from mock import MagicMock
+
+
+
+class TestGreenTestResult(unittest.TestCase):
+
+
+    def setUp(self):
+        self.stream = StringIO()
+
+
+    def tearDown(self):
+        del(self.stream)
+
+
+    def test_startTestVerbose(self):
+        "startTest() contains output we expect in verbose mode"
+        class FakeCase(unittest.TestCase):
+            def runTest(self):
+                pass
+        gtr = GreenTestResult(GreenStream(self.stream), None, 2)
+        tc = FakeCase()
+        gtr.startTest(tc)
+        output = self.stream.getvalue()
+        output_lines = output.split('\n')
+        # Output should look like (I'm not putting the termcolor formatting here)
+        # green.test.test_runner
+        #   FakeCase
+        #     test_it
+        self.assertEqual(len(output_lines), 3)
+        self.assertFalse(' ' in output_lines[0])
+        self.assertTrue('  ' in output_lines[1])
+        self.assertTrue('    ' in output_lines[2])
+
+
+    def test_reportOutcome(self):
+        "_reportOutcome contains output we expect"
+        gtr = GreenTestResult(GreenStream(self.stream), None, 1)
+        gtr._reportOutcome(None, '.', lambda x: x)
+        self.assertTrue('.' in self.stream.getvalue())
+
+
+    def test_reportOutcomeVerbose(self):
+        "_reportOutcome contains output we expect in verbose mode"
+        gtr = GreenTestResult(GreenStream(self.stream), None, 2)
+        l = 'a fake test output line'
+        r = 'a fake reason'
+        gtr.test_output_line = l
+        gtr._reportOutcome(None, '.', lambda x: x, None, r)
+        self.assertTrue(l in self.stream.getvalue())
+        self.assertTrue(r in self.stream.getvalue())
+
+
+    def test_reportOutcomeVerboseHTML(self):
+        "html=True causes _reportOutcome() to escape HTML in docstrings"
+        gtr = GreenTestResult(GreenStream(self.stream), None, 2)
+        gtr.colors.html = True
+        l = 'a fake test output line &nbsp; <>'
+        r = 'a fake reason'
+        gtr.test_output_line = l
+        gtr._reportOutcome(None, '.', lambda x: x, None, r)
+        self.assertTrue(r in self.stream.getvalue())
+        self.assertTrue('&amp;' in self.stream.getvalue())
+        self.assertTrue('&lt;' in self.stream.getvalue())
+        self.assertTrue('&gt;' in self.stream.getvalue())
+        self.assertFalse('&nbsp;' in self.stream.getvalue())
+        self.assertFalse('<' in self.stream.getvalue())
+        self.assertFalse('>' in self.stream.getvalue())
+
+
+    def test_printErrorsDots(self):
+        "printErrors() looks correct in verbose=1 (dots) mode"
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        gtr = GreenTestResult(GreenStream(self.stream), None, 1)
+        test = MagicMock()
+        gtr.addError(test, traceback.format_exception(*err))
+        gtr.printErrors()
+        self.assertTrue('\n\n' in self.stream.getvalue())
+        self.assertTrue('test_printErrorsDots' in self.stream.getvalue())
+        self.assertTrue('raise Exception' in self.stream.getvalue())
+        self.assertTrue('Error' in self.stream.getvalue())
+
+
+    def test_printErrorsVerbose2(self):
+        "printErrors() looks correct in verbose=2 mode"
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        gtr = GreenTestResult(GreenStream(self.stream), None, 2)
+        gtr.test_output_line = "   some test output"
+        test = MagicMock()
+        gtr.addError(test, traceback.format_exception(*err))
+        gtr.printErrors()
+        self.assertTrue('\n\n' in self.stream.getvalue())
+        self.assertTrue('test_printErrorsVerbose2' in self.stream.getvalue())
+        self.assertTrue('raise Exception' in self.stream.getvalue())
+        self.assertTrue('Error' in self.stream.getvalue())
+
+
+    def test_printErrorsVerbose4(self):
+        "printErrors() looks correct in verbose=4 mode"
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        gtr = GreenTestResult(GreenStream(self.stream), None, 4)
+        gtr.test_output_line = "   some test output"
+        test = MagicMock()
+        gtr.addError(test, traceback.format_exception(*err))
+        gtr.printErrors()
+        self.assertTrue('\n\n' in self.stream.getvalue())
+        self.assertTrue('(most recent call last)' in self.stream.getvalue())
+        self.assertTrue('test_printErrorsVerbose4' in self.stream.getvalue())
+        self.assertTrue('raise Exception' in self.stream.getvalue())
+        self.assertTrue('Error' in self.stream.getvalue())
+
+
+    def test_printErrorsZHTML(self):
+        "printErrors() looks correct in html mode"
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        gtr = GreenTestResult(GreenStream(self.stream), None, 4)
+        gtr.colors.html = True
+        gtr.test_output_line = "   some test output"
+        test = MagicMock()
+        gtr.addError(test, traceback.format_exception(*err))
+        gtr.printErrors()
+        self.assertTrue('\n\n' in self.stream.getvalue())
+        self.assertTrue('(most recent call last)' in self.stream.getvalue())
+        self.assertTrue('test_printErrorsZHTML' in self.stream.getvalue())
+        self.assertTrue('raise Exception' in self.stream.getvalue())
+        self.assertTrue('Error' in self.stream.getvalue())
+        self.assertTrue('<span' in self.stream.getvalue())
+        self.assertTrue('color: rgb(' in self.stream.getvalue())
+
+
+
+class TestGreenTestResultAdds(unittest.TestCase):
+
+
+    def setUp(self):
+        self.stream = StringIO()
+        self.gtr = GreenTestResult(GreenStream(self.stream), None, 0)
+        self.gtr._reportOutcome = MagicMock()
+
+
+    def tearDown(self):
+        del(self.stream)
+        del(self.gtr)
+
+
+    def test_addSuccess(self):
+        "addSuccess() makes the correct calls to other functions."
+        test = 'success test'
+        self.gtr.addSuccess(test)
+        self.gtr._reportOutcome.assert_called_with(
+                test, '.', self.gtr.colors.passing)
+
+
+    def test_addError(self):
+        "addError() makes the correct calls to other functions."
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        test = MagicMock()
+        self.gtr.addError(test, err)
+        self.gtr._reportOutcome.assert_called_with(
+                test, 'E', self.gtr.colors.error, err)
+
+
+    def test_addFailure(self):
+        "addFailure() makes the correct calls to other functions."
+        err = None
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        test = MagicMock()
+        self.gtr.addFailure(test, err)
+        self.gtr._reportOutcome.assert_called_with(
+                test, 'F', self.gtr.colors.failing, err)
+
+
+    def test_addSkip(self):
+        "addSkip() makes the correct calls to other functions."
+        test = 'skip test'
+        reason = 'skip reason'
+        self.gtr.addSkip(test, reason)
+        self.gtr._reportOutcome.assert_called_with(
+                test, 's', self.gtr.colors.skipped, reason=reason)
+
+
+    def test_addExpectedFailure(self):
+        "addExpectedFailure() makes the correct calls to other functions."
+        try:
+            raise Exception
+        except:
+            err = sys.exc_info()
+        test = MagicMock()
+        self.gtr.addExpectedFailure(test, err)
+        self.gtr._reportOutcome.assert_called_with(
+                test, 'x', self.gtr.colors.expectedFailure, err)
+
+
+    def test_addUnexpectedSuccess(self):
+        "addUnexpectedSuccess() makes the correct calls to other functions."
+        test = 'unexpected success test'
+        self.gtr.addUnexpectedSuccess(test)
+        self.gtr._reportOutcome.assert_called_with(
+                test, 'u', self.gtr.colors.unexpectedSuccess)
