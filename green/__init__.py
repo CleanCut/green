4f325c7d181c11dd01cf128143838db221cc12c3
Nathan Stocks <nathan.stocks@gmail.com>
2014-06-08 11:31:54 -0600

    Starting coverage at startup didn't work.

--- green/__init__.py
+++ green/__init__.py
@@ -1,11 +1,3 @@
 from .cmdline import main
-import os
-
-try:
-    import coverage
-    os.environ['COVERAGE_PROCESS_START'] = "TRUE"
-    coverage.process_startup()
-except: # pragma: no cover
-    pass
 
 main
