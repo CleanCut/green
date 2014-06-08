b5bad814d01b1f66808a41611ca499321ea8f6ab
Nathan Stocks <nathan.stocks@gmail.com>
2014-06-07 21:41:49 -0600

    Massaging style

--- green/runner.py
+++ green/runner.py
@@ -47,16 +47,26 @@ class SubprocessLogger(object):
 
 
 class DaemonlessProcess(multiprocessing.Process):
-    # make 'daemon' attribute always return False
+
+
     def _get_daemon(self):
         return False
+
+
     def _set_daemon(self, value):
         pass
+
+
+    # 'daemon' attribute needs to always return False
     daemon = property(_get_daemon, _set_daemon)
 
 
 
+
+
 class LoggingDaemonlessPool(Pool):
+
+
     Process = DaemonlessProcess
 
 
