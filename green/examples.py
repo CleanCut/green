8dfedc8616b763a501032ba91c1b0a1f878da5dd
Nathan Stocks <nathan.stocks@gmail.com>
2014-06-22 23:25:46 -0600

    Added docstrings.  Added delayed tests, but commented them out.

--- green/examples.py
+++ green/examples.py
@@ -7,28 +7,50 @@ class TestStates(unittest.TestCase):
 
 
     def test0Pass(self):
+        "This test will pass"
         return
 
 
     def test1Fail(self):
+        "This test will fail an assertion"
         self.assertTrue(False)
 
 
     def test2Error(self):
+        "An Exception will be raised (and not caught) while running this test."
         raise Exception
 
 
-    @unittest.skip("Testing skip functionality.")
+    @unittest.skip("This is the 'reason' portion of the skipped test.")
     def test3Skip(self):
+        "This test will be skipped."
         pass
 
     @unittest.expectedFailure
     def test4ExpectedFailure(self):
+        "This test will fail, but we expect it to."
         self.assertEqual(True, False)
 
 
     @unittest.expectedFailure
     def test5UnexpectedPass(self):
+        "This test will pass, but we expected it to fail!"
         pass
 
 
+#    def test6SmallDelay(self):
+#        "This test will take 1 seconds to pass."
+#        import time
+#        time.sleep(1)
+#
+#
+#    def test7BigDelay(self):
+#        "This test will take 2 seconds to pass."
+#        import time
+#        time.sleep(2)
+#
+#
+#    def test8AnotherDelay(self):
+#        "This test will also take 2 seconds to pass, but not if you use 3 or more subprocesses!"
+#        import time
+#        time.sleep(2)
