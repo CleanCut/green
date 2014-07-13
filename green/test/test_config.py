d5e1710782201a34e1145b8c2bd4db9f0a3cc9b3
Tom Barron <tom.barron@comcast.net>
2014-07-12 21:02:25 -0400

    Reset default config in setUp rather than tearDown

--- green/test/test_config.py
+++ green/test/test_config.py
@@ -5,7 +5,7 @@ import sys
 import shutil
 import tempfile
 import unittest
-import config
+from green import config
 
 """
 Testcases:
@@ -72,6 +72,8 @@ class env_excursion(object):
 class TestConfig(unittest.TestCase):
 
     def setUp(self):
+        if hasattr(config.get_config, "_cfg"):
+            del config.get_config._cfg
         self.tmpd = tempfile.mkdtemp()
         self.defname = os.path.join(self.tmpd, ".green")
         self.write_file(self.defname,
@@ -103,8 +105,6 @@ class TestConfig(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(self.tmpd)
-        if hasattr(config.get_config, "_cfg"):
-            del config.get_config._cfg
 
     def test_cmd_env_def(self):
         """
