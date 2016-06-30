ea7c7066cf07855f83960e63e3d756c2c6f16fc9
Nathan Stocks <nathan.stocks@gmail.com>
2016-06-30 12:29:38 -0600

    Because of order-of-importing, the version module shows up as uncovered, even though it's covered.  Guess we'll have to pragma it.

--- green/version.py
+++ green/version.py
@@ -1,19 +1,19 @@
-from __future__ import unicode_literals
-import os.path
-import sys
+from __future__ import unicode_literals # progma nocover
+import os.path # progma nocover
+import sys # progma nocover
 
-try:
+try: # progma nocover
     import coverage
 except: # pragma nocover
     coverage = None
 
-__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read().strip()
+__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read().strip() # progma nocover
 if sys.version_info[0] == 2: # pragma nocover
     from __builtin__ import unicode  # just so the linter stops complaining
     __version__ = unicode(__version__)
 
 
-def pretty_version():
+def pretty_version(): # progma nocover
     python_version = ".".join([str(x) for x in sys.version_info[0:3]])
     ver_str = "Green {}".format(__version__)
     if coverage:
