8f75d022f41cb3fca5a5eec50f7d1ef38f018b86
Nathan Stocks <nathan.stocks@gmail.com>
2016-06-30 13:09:06 -0600

    Fix the silly typos I made.

--- green/version.py
+++ green/version.py
@@ -1,19 +1,19 @@
-from __future__ import unicode_literals # progma nocover
-import os.path # progma nocover
-import sys # progma nocover
+from __future__ import unicode_literals # pragma: no cover
+import os.path # pragma nocover
+import sys # pragma nocover
 
-try: # progma nocover
+try: # pragma nocover
     import coverage
 except: # pragma nocover
     coverage = None
 
-__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read().strip() # progma nocover
+__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read().strip() # pragma nocover
 if sys.version_info[0] == 2: # pragma nocover
     from __builtin__ import unicode  # just so the linter stops complaining
     __version__ = unicode(__version__)
 
 
-def pretty_version(): # progma nocover
+def pretty_version(): # pragma nocover
     python_version = ".".join([str(x) for x in sys.version_info[0:3]])
     ver_str = "Green {}".format(__version__)
     if coverage:
