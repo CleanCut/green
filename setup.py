5dcc2cd0e4e36f5401d8fdf418f25ec63e6820bc
MinchinWeb <w_minchin@hotmail.com>
2016-10-28 22:27:34 -0600

    Use built in function to get terminal size

--- setup.py
+++ setup.py
@@ -24,6 +24,13 @@ setup(
     package_data = {'green' : ['VERSION', 'shell_completion.sh']},
     version = version,
     install_requires = dependencies,
+    extras_require = {
+        ':sys_platform=="win32"': ['colorama>=0.2.5'],
+        # shutil.get_terminal_size() introduced in Python 3.3
+        ':python_version=="2.7"': ['backports.shutil_get_terminal_size>=1.0.0'],
+        # add here for PyPy3
+        ':python_version=="3.3"': ['backports.shutil_get_terminal_size>=1.0.0'],
+    },
     entry_points = {
         'console_scripts' : [
             'green = green:main',
