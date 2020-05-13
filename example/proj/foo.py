d9ec300af85d70c87e9edad3bfc713e0cf16b209
bkmd11 <bkendall11@gmail.com>
2020-05-13 05:53:05 -0400

    undoing some changes made by accident

---
+++ example/proj/foo.py
@@ -0,0 +1,19 @@
+def answer():
+    """
+    >>> answer()
+    42
+    """
+    return 42
+
+class School():
+
+    def food(self):
+        """
+        >>> s = School()
+        >>> s.food()
+        'awful'
+        """
+        return 'awful'
+
+    def age(self):
+        return 300
