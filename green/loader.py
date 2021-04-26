ed49351313f26a80e71503d5c7071ffed22653c9
Nathan Stocks <cleancut@github.com>
2021-04-25 19:28:08 -0600

    Fix a couple new failing lints

--- green/loader.py
+++ green/loader.py
@@ -191,7 +191,6 @@ class GreenTestLoader(unittest.TestLoader):
         return flattenTestSuite(suites) if suites else None
 
     def loadTarget(self, target, file_pattern="test*.py"):
-        """"""
         debug(
             "Attempting to load target '{}' with file_pattern '{}'".format(
                 target, file_pattern
