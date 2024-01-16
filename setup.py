bc6b69c27898c618de012ecdc387f55843199797
Stephane Odul <stephane@clumio.com>
2024-01-16 12:46:52 -0800

    Explicitly add pypy 3.8, 3.9 and 3.10 to CI. We only support pypy as 'best effort'.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #
 # setup.py file for cleancut/green.
 #
-# setuptools versions older than 64.0.0 need a minimal setup.py file.g
+# setuptools versions older than 64.0.0 need a minimal setup.py file.
 #
 
 from setuptools import setup
