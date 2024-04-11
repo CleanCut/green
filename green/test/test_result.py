CleanCut/green

commit 651ab467e371b42e6205b9c3d223f2e1f50e4b0a

* Add type annotation. * fix/flag potential bugs highlighted by mypy.

green/test/test_result.py
@@ -44,7 +44,7 @@ class TestBaseTestResult(unittest.TestCase):
         """
         recordStdout records output.
         """
-        btr = BaseTestResult(None, None)
+        btr = BaseTestResult(None, colors=None)
         pt = ProtoTest()
         o = "some output"
         btr.recordStdout(pt, o)
@@ -54,7 +54,7 @@ class TestBaseTestResult(unittest.TestCase):
         """
         recordStdout ignores empty output sent to it
         """
-        btr = BaseTestResult(None, None)
+        btr = BaseTestResult(None, colors=None)
         pt = ProtoTest()
         btr.recordStdout(pt, "")
         self.assertEqual(btr.stdout_output, {})
@@ -65,7 +65,7 @@ class TestBaseTestResult(unittest.TestCase):
         """
         stream = StringIO()
         noise = "blah blah blah"
-        btr = BaseTestResult(stream, Colors(False))
+        btr = BaseTestResult(stream, colors=Colors(False))
         pt = ProtoTest()
         btr.stdout_output[pt] = noise
         btr.displayStdout(pt)
@@ -75,7 +75,7 @@ class TestBaseTestResult(unittest.TestCase):
         """
         recordStderr records errput.
         """
-        btr = BaseTestResult(None, None)
+        btr = BaseTestResult(None, colors=None)
         pt = ProtoTest()
         o = "some errput"
         btr.recordStderr(pt, o)
@@ -85,7 +85,7 @@ class TestBaseTestResult(unittest.TestCase):
         """
         recordStderr ignores empty errput sent to it
         """
-        btr = BaseTestResult(None, None)
+        btr = BaseTestResult(None, colors=None)
         pt = ProtoTest()
         btr.recordStderr(pt, "")
         self.assertEqual(btr.stderr_errput, {})
@@ -96,7 +96,7 @@ class TestBaseTestResult(unittest.TestCase):
         """
         stream = StringIO()
         noise = "blah blah blah"
-        btr = BaseTestResult(stream, Colors(False))
+        btr = BaseTestResult(stream, colors=Colors(False))
         pt = ProtoTest()
         btr.stderr_errput[pt] = noise
         btr.displayStderr(pt)

green/loader.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections import OrderedDict
 from doctest import DocTestSuite
 from fnmatch import fnmatch
@@ -12,7 +14,7 @@ import unittest
 import traceback
 
 from green.output import debug
-from green.result import proto_test
+from green import result
 from green.suite import GreenTestSuite
 
 python_file_pattern = re.compile(r"^[_a-z]\w*?\.py$", re.IGNORECASE)
@@ -293,7 +295,9 @@ class GreenTestLoader(unittest.TestLoader):
         return None
 
 
-def toProtoTestList(suite, test_list=None, doing_completions=False):
+def toProtoTestList(
+    suite, test_list=None, doing_completions: bool = False
+) -> list[result.ProtoTest]:
     """
     Take a test suite and turn it into a list of ProtoTests.
 
@@ -314,22 +318,22 @@ def toProtoTestList(suite, test_list=None, doing_completions=False):
     if isinstance(suite, unittest.TestCase):
         # Skip actual blank TestCase objects that twisted inserts
         if str(type(suite)) != "<class 'twisted.trial.unittest.TestCase'>":
-            test_list.append(proto_test(suite))
+            test_list.append(result.proto_test(suite))
     else:
         for i in suite:
             toProtoTestList(i, test_list, doing_completions)
     return test_list
 
 
-def toParallelTargets(suite, targets):
+def toParallelTargets(suite, targets: list[str]) -> list[str]:
     """
     Produce a list of targets which should be tested in parallel.
 
-    For the most part this will be a list of test modules.  The exception is
-    when a dotted name representing something more granular than a module
-    was input (like an individal test case or test method)
+    For the most part, this will be a list of test modules.
+    The exception is when a dotted name representing something more granular
+    than a module was input (like an individual test case or test method).
     """
-    targets = filter(lambda x: x != ".", targets)
+    targets = [x for x in targets if x != "."]
     # First, convert the suite to a proto test list - proto tests nicely
     # parse things like the fully dotted name of the test and the
     # finest-grained module it belongs to, which simplifies our job.
@@ -338,11 +342,12 @@ def toParallelTargets(suite, targets):
     modules = {x.module for x in proto_test_list}
     # Get the list of user-specified targets that are NOT modules
     non_module_targets = []
+    target: str
     for target in targets:
-        if not list(filter(None, [target in x for x in modules])):
+        if not list(filter(None, (target in x for x in modules))):
             non_module_targets.append(target)
     # Main loop -- iterating through all loaded test methods
-    parallel_targets = []
+    parallel_targets: list[str] = []
     for test in proto_test_list:
         found = False
         for target in non_module_targets:

green/output.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Iterable, TYPE_CHECKING
+
 from colorama import Fore, Style
 from colorama.ansi import Cursor
 from colorama.initialise import wrap_stream
@@ -8,6 +12,10 @@ import re
 import sys
 from unidecode import unidecode
 
+if TYPE_CHECKING:
+    from colorama.ansitowin32 import StreamWrapper
+    from colorama.initialise import _TextIOT
+
 global debug_level
 debug_level = 0
 
@@ -15,7 +23,7 @@ text_type = str
 unicode = None  # so pyflakes stops complaining
 
 
-def debug(message, level=1):
+def debug(message: str, level: int = 1):
     """
     So we can tune how much debug output we get when we turn it on.
     """
@@ -28,73 +36,72 @@ class Colors:
     A class to centralize wrapping strings in terminal colors.
     """
 
-    def __init__(self, termcolor=None):
-        """
-        termcolor - If None, attempt to autodetect whether we are in a terminal
-            and turn on terminal colors if we think we are.  If True, force
-            terminal colors on.  If False, force terminal colors off.
-        """
-        if termcolor is None:
-            self.termcolor = sys.stdout.isatty()
-        else:
-            self.termcolor = termcolor
+    def __init__(self, termcolor: bool | None = None):
+        """Initialize the Colors object.
 
-    def wrap(self, text, style):
+        Args:
+            termcolor: If None, attempt to autodetect whether we are in a
+                terminal and turn on terminal colors if we think we are.
+                If True, force terminal colors on.
+                If False, force terminal colors off.
+        """
+        self.termcolor = sys.stdout.isatty() if termcolor is None else termcolor
+
+    def wrap(self, text: str, style: str) -> str:
         if self.termcolor:
             return f"{style}{text}{Style.RESET_ALL}"
-        else:
-            return text
+        return text
 
     # Movement
-    def start_of_line(self):
+    def start_of_line(self) -> str:
         return "\r"
 
-    def up(self, lines=1):
+    def up(self, lines: int = 1) -> str:
         return Cursor.UP(lines)
 
     # Real colors and styles
-    def bold(self, text):
+    def bold(self, text: str) -> str:
         return self.wrap(text, Style.BRIGHT)
 
-    def blue(self, text):
+    def blue(self, text: str) -> str:
         if platform.system() == "Windows":  # pragma: no cover
             # Default blue in windows is unreadable (such awful defaults...)
             return self.wrap(text, Fore.CYAN)
         else:
             return self.wrap(text, Fore.BLUE)
 
-    def green(self, text):
+    def green(self, text: str) -> str:
         return self.wrap(text, Fore.GREEN)
 
-    def red(self, text):
+    def red(self, text: str) -> str:
         return self.wrap(text, Fore.RED)
 
-    def yellow(self, text):
+    def yellow(self, text: str) -> str:
         return self.wrap(text, Fore.YELLOW)
 
     # Abstracted colors and styles
-    def passing(self, text):
+    def passing(self, text: str) -> str:
         return self.green(text)
 
-    def failing(self, text):
+    def failing(self, text: str) -> str:
         return self.red(text)
 
-    def error(self, text):
+    def error(self, text: str) -> str:
         return self.red(text)
 
-    def skipped(self, text):
+    def skipped(self, text: str) -> str:
         return self.blue(text)
 
-    def unexpectedSuccess(self, text):
+    def unexpectedSuccess(self, text: str) -> str:
         return self.yellow(text)
 
-    def expectedFailure(self, text):
+    def expectedFailure(self, text: str) -> str:
         return self.yellow(text)
 
-    def moduleName(self, text):
+    def moduleName(self, text: str) -> str:
         return self.bold(text)
 
-    def className(self, text):
+    def className(self, text: str) -> str:
         return text
 
 
@@ -111,19 +118,19 @@ class GreenStream:
            writelines(lines)
     """
 
-    indent_spaces = 2
-    _ascii_only_output = False  # default to printing output in unicode
+    indent_spaces: int = 2
+    _ascii_only_output: bool = False  # default to printing output in unicode
     coverage_pattern = re.compile(r"TOTAL\s+\d+\s+\d+\s+(?P<percent>\d+)%")
 
     def __init__(
         self,
-        stream,
-        override_appveyor=False,
-        disable_windows=False,
-        disable_unidecode=False,
-    ):
+        stream: _TextIOT,
+        override_appveyor: bool = False,
+        disable_windows: bool = False,
+        disable_unidecode: bool = False,
+    ) -> None:
         self.disable_unidecode = disable_unidecode
-        self.stream = stream
+        self.stream: _TextIOT | StreamWrapper = stream
         # Ironically, Windows CI platforms such as GitHub Actions and AppVeyor don't support windows
         # win32 system calls for colors, but it WILL interpret posix ansi escape codes! (The
         # opposite of an actual windows command prompt)
@@ -135,7 +142,7 @@ class GreenStream:
         if override_appveyor or (
             (on_windows and not on_windows_ci) and not disable_windows
         ):  # pragma: no cover
-            self.stream = wrap_stream(self.stream, None, None, None, True)
+            self.stream = wrap_stream(stream, None, None, False, True)
             # set output is ascii-only
             self._ascii_only_output = True
         self.closed = False
@@ -147,23 +154,23 @@ class GreenStream:
         # Susceptible to false-positives if other matching lines are output,
         # so set this to None immediately before running a coverage report to
         # guarantee accuracy.
-        self.coverage_percent = None
+        self.coverage_percent: int | None = None
 
-    def flush(self):
+    def flush(self) -> None:
         self.stream.flush()
 
-    def writeln(self, text=""):
+    def writeln(self, text: str = "") -> None:
         self.write(text + "\n")
 
-    def write(self, text):
-        if type(text) == bytes:
+    def write(self, text: str) -> None:
+        if isinstance(text, bytes):
             text = text.decode("utf-8")
         # Compensate for windows' anti-social unicode behavior
         if self._ascii_only_output and not self.disable_unidecode:
             # Windows doesn't actually want unicode, so we get
             # the closest ASCII equivalent
             text = text_type(unidecode(text))
-        # Since coverage doesn't like us switching out it's stream to run extra
+        # Since coverage doesn't like us switching out its stream to run extra
         # reports to look for percent covered. We should replace this with
         # grabbing the percentage directly from coverage if we can figure out
         # how.
@@ -174,14 +181,14 @@ class GreenStream:
                 self.coverage_percent = int(percent_str)
         self.stream.write(text)
 
-    def writelines(self, lines):
+    def writelines(self, lines: Iterable[str]) -> None:
         """
         Just for better compatibility with real file objects
         """
         for line in lines:
             self.write(line)
 
-    def formatText(self, text, indent=0, outcome_char=""):
+    def formatText(self, text: str, indent: int = 0, outcome_char: str = "") -> str:
         # We'll go through each line in the text, modify it, and store it in a
         # new list
         updated_lines = []
@@ -197,7 +204,7 @@ class GreenStream:
         output = "\n".join(updated_lines)
         return output
 
-    def formatLine(self, line, indent=0, outcome_char=""):
+    def formatLine(self, line: str, indent: int = 0, outcome_char: str = "") -> str:
         """
         Takes a single line, optionally adds an indent and/or outcome
         character to the beginning of the line.
@@ -205,7 +212,7 @@ class GreenStream:
         actual_spaces = (indent * self.indent_spaces) - len(outcome_char)
         return outcome_char + " " * actual_spaces + line
 
-    def isatty(self):
+    def isatty(self) -> bool:
         """
         Wrap internal self.stream.isatty.
         """

green/process.py
@@ -1,10 +1,14 @@
+from __future__ import annotations
+
 import logging
 import multiprocessing
 from multiprocessing.pool import Pool
+import os
 import random
 import sys
 import tempfile
 import traceback
+from typing import Type, TYPE_CHECKING, Union, Tuple, Callable, Iterable
 
 import coverage
 
@@ -13,20 +17,28 @@ from green.loader import GreenTestLoader
 from green.result import proto_test, ProtoTest, ProtoTestResult
 
 
+if TYPE_CHECKING:
+    from types import TracebackType
+    from queue import Queue
+
+    ExcInfoType = Union[
+        Tuple[Type[BaseException], BaseException, TracebackType],
+        Tuple[None, None, None],
+    ]
+
+
 # Super-useful debug function for finding problems in the subprocesses, and it
 # even works on windows
-def ddebug(msg, err=None):  # pragma: no cover
+def ddebug(msg: str, err: ExcInfoType | None = None):  # pragma: no cover
     """
     err can be an instance of sys.exc_info() -- which is the latest traceback
     info
     """
-    import os
-
     if err:
-        err = "".join(traceback.format_exception(*err))
+        error_string = "".join(traceback.format_exception(*err))
     else:
-        err = ""
-    sys.__stdout__.write(f"({os.getpid()}) {msg} {err}\n")
+        error_string = ""
+    sys.__stdout__.write(f"({os.getpid()}) {msg} {error_string}\n")
     sys.__stdout__.flush()
 
 
@@ -36,7 +48,7 @@ class ProcessLogger:
     instead of having process crashes be silent.
     """
 
-    def __init__(self, callable):
+    def __init__(self, callable: Callable):
         self.__callable = callable
 
     def __call__(self, *args, **kwargs):
@@ -65,13 +77,16 @@ class LoggingDaemonlessPool(Pool):
     """
 
     @staticmethod
-    def Process(ctx, *args, **kwds):
-        process = ctx.Process(daemon=False, *args, **kwds)
+    def Process(ctx, *args, **kwargs):
+        process = ctx.Process(daemon=False, *args, **kwargs)
         return process
 
-    def apply_async(self, func, args=(), kwds={}, callback=None, error_callback=None):
+    # FIXME: `kwargs={}` is dangerous as the empty dict is declared at import time
+    # and becomes a shared object between all instances of LoggingDaemonlessPool.
+    # In short, it is a global variable that is mutable.
+    def apply_async(self, func, args=(), kwargs={}, callback=None, error_callback=None):
         return Pool.apply_async(
-            self, ProcessLogger(func), args, kwds, callback, error_callback
+            self, ProcessLogger(func), args, kwargs, callback, error_callback
         )
 
     _wrap_exception = True
@@ -212,27 +227,27 @@ def worker(
 
 # Unmodified (see above)
 class RemoteTraceback(Exception):  # pragma: no cover
-    def __init__(self, tb):
+    def __init__(self, tb: str):
         self.tb = tb
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.tb
 
 
 # Unmodified (see above)
 class ExceptionWithTraceback:  # pragma: no cover
-    def __init__(self, exc, tb):
-        tb = traceback.format_exception(type(exc), exc, tb)
-        tb = "".join(tb)
+    def __init__(self, exc: BaseException, tb: TracebackType):
+        tb_lines = traceback.format_exception(type(exc), exc, tb)
+        tb_text = "".join(tb_lines)
         self.exc = exc
-        self.tb = '\n"""\n%s"""' % tb
+        self.tb = '\n"""\n%s"""' % tb_text
 
-    def __reduce__(self):
+    def __reduce__(self) -> Tuple[Callable, Tuple[BaseException, str]]:
         return rebuild_exc, (self.exc, self.tb)
 
 
 # Unmodified (see above)
-def rebuild_exc(exc, tb):  # pragma: no cover
+def rebuild_exc(exc: BaseException, tb: str):  # pragma: no cover
     exc.__cause__ = RemoteTraceback(tb)
     return exc
 
@@ -242,8 +257,13 @@ multiprocessing.pool.worker = worker  # type: ignore
 # -----------------------------------------------------------------------------
 
 
+# Fixme: `omit_patterns=[]` is a global mutable.
 def poolRunner(
-    target, queue, coverage_number=None, omit_patterns=[], cov_config_file=True
+    target: str,
+    queue: Queue,
+    coverage_number: int | None = None,
+    omit_patterns: str | Iterable[str] | None = [],
+    cov_config_file: bool = True,
 ):  # pragma: no cover
     """
     I am the function that pool worker processes run.  I run one unit test.
@@ -259,7 +279,7 @@ def poolRunner(
     saved_tempdir = tempfile.tempdir
     tempfile.tempdir = tempfile.mkdtemp()
 
-    def raise_internal_failure(msg):
+    def raise_internal_failure(msg: str):
         err = sys.exc_info()
         t = ProtoTest()
         t.module = "green.loader"
@@ -361,7 +381,7 @@ def poolRunner(
                 target, str(test), type(test), dir(test)
             )
         )
-        err = (TypeError, TypeError(description), None)
+        no_run_error = (TypeError, TypeError(description), None)
         t = ProtoTest()
         target_list = target.split(".")
         t.module = ".".join(target_list[:-2]) if len(target_list) > 1 else target
@@ -371,7 +391,7 @@ def poolRunner(
             target.split(".")[-1] if len(target_list) > 1 else "unknown_method"
         )
         result.startTest(t)
-        result.addError(t, err)
+        result.addError(t, no_run_error)
         result.stopTest(t)
         queue.put(result)
 

green/runner.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import multiprocessing
 from sys import modules
+from typing import TYPE_CHECKING
 from unittest.signals import registerResult, installHandler, removeResult
 import warnings
 
@@ -7,7 +10,11 @@ from green.exceptions import InitializerOrFinalizerError
 from green.loader import toParallelTargets
 from green.output import debug, GreenStream
 from green.process import LoggingDaemonlessPool, poolRunner
-from green.result import GreenTestResult
+from green.result import GreenTestResult, ProtoTestResult
+
+if TYPE_CHECKING:
+    from multiprocessing.managers import SyncManager
+    from queue import Queue
 
 
 class InitializerOrFinalizer:
@@ -49,7 +56,7 @@ class InitializerOrFinalizer:
             )
 
 
-def run(suite, stream, args, testing=False):
+def run(suite, stream, args, testing: bool = False) -> GreenTestResult:
     """
     Run the given test case or test suite with the specified arguments.
 
@@ -66,7 +73,8 @@ def run(suite, stream, args, testing=False):
     # Note: Catching SIGINT isn't supported by Python on windows (python
     # "WONTFIX" issue 18040)
     installHandler()
-    registerResult(result)
+    # Ignore the type mismatch until we make GreenTestResult a subclass of unittest.TestResult.
+    registerResult(result)  # type: ignore
 
     with warnings.catch_warnings():
         if args.warnings:  # pragma: no cover
@@ -95,8 +103,10 @@ def run(suite, stream, args, testing=False):
             finalizer=InitializerOrFinalizer(args.finalizer),
             maxtasksperchild=args.maxtasksperchild,
         )
-        manager = multiprocessing.Manager()
-        targets = [(target, manager.Queue()) for target in parallel_targets]
+        manager: SyncManager = multiprocessing.Manager()
+        targets: list[tuple[str, Queue]] = [
+            (target, manager.Queue()) for target in parallel_targets
+        ]
         if targets:
             for index, (target, queue) in enumerate(targets):
                 if args.run_coverage:
@@ -131,7 +141,7 @@ def run(suite, stream, args, testing=False):
                         # currently waiting on this test, so print out
                         # the white 'processing...' version of the output
                         result.startTest(msg)
-                        proto_test_result = queue.get()
+                        proto_test_result: ProtoTestResult = queue.get()
                         debug(
                             "runner.run(): received proto test result: {}".format(
                                 str(proto_test_result)
@@ -153,6 +163,7 @@ def run(suite, stream, args, testing=False):
 
         result.stopTestRun()
 
-    removeResult(result)
+    # Ignore the type mismatch untile we make GreenTestResult a subclass of unittest.TestResult.
+    removeResult(result)  # type: ignore
 
     return result

commit b671f6282f034199bef5870b31537f1b52dd582b

Fix test_time being always 0.0
The bug was introduced in commit 651ab467e371b42e6205b9c3d223f2e1f50e4b0a
when setting start_time = 0.0 was added to ProtoTestResult.reinitialize().

green/result.py
@@ -329,8 +329,8 @@ class ProtoTestResult(BaseTestResult):
         Called before each test runs.
         """
         test = proto_test(test)
-        self.start_time = time.time()
         self.reinitialize()
+        self.start_time = time.time()
         if self.start_callback:
             self.start_callback(test)
 

green/test/test_process.py
@@ -103,10 +103,12 @@ class TestPoolRunner(unittest.TestCase):
         )
         fh.close()
         module_name = basename + ".test_pool_runner_dotted.A.testPass"
-        result = Queue()
-        poolRunner(module_name, result, 1)
-        result.get()
-        self.assertEqual(len(result.get().passing), 1)
+        results = Queue()
+        poolRunner(module_name, results, 1)
+        results.get()
+        result = results.get()
+        self.assertEqual(len(result.passing), 1)
+        self.assertGreater(float(result.test_time), 0)
 
     def test_SyntaxErrorInUnitTest(self):
         """

green/test/test_result.py
@@ -104,6 +104,16 @@ class TestBaseTestResult(unittest.TestCase):
 
 
 class TestProtoTestResult(unittest.TestCase):
+    def test_startStop(self):
+        """
+        startTest/stopTest work correctly
+        """
+        ptr = ProtoTestResult()
+        test = proto_test(MagicMock())
+        ptr.startTest(test)
+        ptr.stopTest(test)
+        self.assertGreater(float(ptr.test_time), 0)
+
     def test_addSuccess(self):
         """
         addSuccess adds a test correctly